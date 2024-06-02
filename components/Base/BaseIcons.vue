1773e2dbfbeac0cb90f6a6e7a2ce2f8008aa6b26
Dariusz Legizynski <d.legizynski@.com>
2024-06-02 21:41:56 +0200

    first draft of services section

--- components/Base/BaseIcons.vue
+++ components/Base/BaseIcons.vue
@@ -40,4 +40,33 @@
 			/>
 		</g>
 	</svg>
+	<svg v-if="type === 'document-3'" xmlns="http://www.w3.org/2000/svg" :width="width" :height="height" :fill="fill" viewBox="0 0 48 48">
+		<g><path fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" d="M5.5 5.5v37H24V24h18.5V5.5ZM24 42.5 42.5 24" /></g>
+	</svg>
+	<svg v-if="type === 'pen'" xmlns="http://www.w3.org/2000/svg" :width="width" :height="height" :fill="fill" viewBox="0 0 48 48">
+		<g id="SVGRepo_iconCarrier">
+			<path
+				fill="none"
+				stroke="#000"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M4.5 36.927c5.402-2.741 5.402-2.786 7.865-3.19-1.124 1.491-2.405 2.435-1.888 5.347 3.052.634 5.968-1.739 8.629-2.633-.8 1.034-2.094 3.168-.517 3.595a5.672 5.672 0 0 0 2.67-.462M26.059 35.879l-3.371 2.43 1.093-3.954"
+				class="c"
+			/>
+			<path
+				d="m35.728 22.343-9.67 13.536-2.277-1.524L32.51 20.29m0 0L41.311 7.94c.922.026 1.76.548 2.189 1.364l-7.772 13.04-3.219-2.055ZM42.809 10.597c.377 3.184-2.869 8.258-4.217 10.806"
+			/>
+		</g>
+	</svg>
+	<svg v-if="type === 'write'" xmlns="http://www.w3.org/2000/svg" :width="width" :height="height" :fill="fill" viewBox="0 0 48 48">
+		<g id="SVGRepo_iconCarrier">
+			<path
+				fill="none"
+				stroke="#000"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M37.31 15.93 12.24 41 5.5 42.5 7 35.76l.51-.51 28-28 .51-.51 1.76 1.76.55-.5 2.46-2.5h0l1.71 1.71L40 9.68l-.52.51 1.8 1.81-3 3 1.41 1.42 1.45 1.44-4.88 4.88M37.81 8.48l1.71 1.71M38.31 14.93l-1 1M7 35.76 12.24 41"
+			/>
+		</g>
+	</svg>
 </template>
--- components/Home/HomeServices.vue
+++ components/Home/HomeServices.vue
@@ -1,46 +1,35 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <article class="mt-28">
-    <section>
-      <div class="h3 text-primary text-center uppercase">
-        Sprachdienste im Angebot
-      </div>
-      <h1 class="uppercase tracking-[3px] text-center my-6">Meine Leistungen</h1>
-      <section>
-        <div>
-          <BaseIcons type="document" width="6rem" height="6rem" />
-          <h2>Übersetzungen</h2>
-          <p>
-            Ich biete professionelle Übersetzungen in den Sprachen Deutsch, Englisch, Polnisch und Russisch an.
-          </p>
-        </div>
-        <div>
-          <div>ICON</div>
-          <h2>Dolmetschen</h2>
-          <p>
-            Ich biete professionelle Dolmetscherdienste in den Sprachen Deutsch, Englisch, Polnisch und Russisch an.
-          </p>
-        </div>
-        <div>
-          <div>ICON</div>
-          <h2>Texterstellung</h2>
-          <p>
-            Ich biete professionelle Texterstellung in den Sprachen Deutsch, Englisch, Polnisch und Russisch an.
-          </p>
-        </div>
-        <div>
-          <div>ICON</div>
-          <h2>Textkorrektur</h2>
-          <p>
-            Ich biete professionelle Textkorrektur in den Sprachen Deutsch, Englisch, Polnisch und Russisch an.
-          </p>
-        </div>
-      </section>
-      <BaseButton>Kontakieren Sie mich</BaseButton>
-    </section>
-
-  </article>
+	<article class="mt-28">
+		<section>
+			<div class="h3 text-primary text-center uppercase">Sprachdienste im Angebot</div>
+			<h1 class="uppercase tracking-[3px] text-center my-6">Meine Leistungen</h1>
+			<section class="px-4">
+				<div class="flex flex-col items-center my-16">
+					<BaseIcons type="document" width="6rem" height="6rem" />
+					<h2 class="my-4">Übersetzungen</h2>
+					<span>Ich biete professionelle Übersetzungen in den Sprachen Deutsch, Englisch, Polnisch und Russisch an.</span>
+				</div>
+				<div class="flex flex-col items-center my-16">
+					<BaseIcons type="pen" width="6rem" height="6rem" />
+					<h2 class="my-4">Dolmetschen</h2>
+					<span>Ich biete spanrofessionelle Dolmetscherdienste in den Sprachen Deutsch, Englisch, Polnisch und Russisch an.</span>
+				</div>
+				<div class="flex flex-col items-center my-16">
+					<BaseIcons type="document-2" width="6rem" height="6rem" />
+					<h2 class="my-4">Texterstellung</h2>
+					<span>Ich biete professionelle Texterstellung in den Sprachen Deutsch, Englisch, Polnisch und Russisch an.</span>
+				</div>
+				<div class="flex flex-col items-center my-16">
+					<BaseIcons type="write" width="6rem" height="6rem" />
+					<h2 class="my-4">Textkorrektur</h2>
+					<span>Ich biete professionelle Textkorrektur in den Sprachen Deutsch, Englisch, Polnisch und Russisch an.</span>
+				</div>
+			</section>
+			<section class="flex flex-col items-center mb-16">
+				<BaseButton>Więcej</BaseButton>
+			</section>
+		</section>
+	</article>
 </template>
