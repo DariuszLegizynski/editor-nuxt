c75dea482c8c7c240650cd7d75cc618311d6119e
Dariusz Legizynski <d.legizynski@.com>
2024-06-03 23:53:57 +0200

    start styling

--- pages/services.vue
+++ pages/services.vue
@@ -1,16 +1,37 @@
 <template>
-	<article>
-		<section class="p-2 h-[90vh] flex flex-col items-center justify-end">
-			<p>Pracuję i pomagam w języku niemieckim (AT, DE i CH)</p>
-			<p>Oferuję następujące usługi:</p>
-			<ul>
-				<li><p>korekta i edycja pism</p></li>
-				<li><p>tłumaczenie dokumentów i pomoc w ich zrozumieniu</p></li>
-				<li><p>odpowiadanie na pisma urzędowe</p></li>
-				<li><p>pomoc sprawach publicznych</p></li>
-				<li><p>przepisywanie pism</p></li>
-				<li><p>sprawdzenie przygotowanej dokumentacji</p></li>
-			</ul>
+	<article class="h-screen flex flex-col items-center">
+		<section class="my-36">
+			<h1 class="text-[4.4rem] text-primary">Usługi</h1>
+			<div class="flex">
+				<h1 class="text-[4.4rem] decorative">&</h1>
+				<h1 class="text-[4.4rem] text-primary">&nbsp;Ceny</h1>
+			</div>
+		</section>
+		<section class="px-4 grid gap-y-1">
+			<div class="grid grid-cols-[auto_auto] gap-x-4 justify-between">
+				<p>korekta i edycja pism</p>
+				<p>100&nbsp;€</p>
+			</div>
+			<div class="grid grid-cols-[auto_auto] gap-x-4 justify-between">
+				<p>tłumaczenie dokumentów i pomoc w ich zrozumieniu</p>
+				<p>100&nbsp;€</p>
+			</div>
+			<div class="grid grid-cols-[auto_auto] gap-x-4 justify-between">
+				<p>odpowiadanie na pisma urzędowe</p>
+				<p>100&nbsp;€</p>
+			</div>
+			<div class="grid grid-cols-[auto_auto] gap-x-4 justify-between">
+				<p>pomoc sprawach publicznych</p>
+				<p>100&nbsp;€</p>
+			</div>
+			<div class="grid grid-cols-[auto_auto] gap-x-4 justify-between">
+				<p>przepisywanie pism</p>
+				<p>100&nbsp;€</p>
+			</div>
+			<div class="grid grid-cols-[auto_auto] gap-x-4 justify-between">
+				<p>sprawdzenie przygotowanej dokumentacji</p>
+				<p>100&nbsp;€</p>
+			</div>
 		</section>
 	</article>
 </template>
