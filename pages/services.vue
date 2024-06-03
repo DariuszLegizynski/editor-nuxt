311cc32d12f81b6d935a55d0347650effd7f23e4
Dariusz Legizynski <d.legizynski@.com>
2024-06-03 15:53:36 +0200

    make first draft

---
+++ pages/services.vue
@@ -0,0 +1,16 @@
+<template>
+	<article>
+		<section class="p-2 h-[90vh] flex flex-col items-center justify-end">
+			<p>Pracuję i pomagam w języku niemieckim (AT, DE i CH)</p>
+			<p>Oferuję następujące usługi:</p>
+			<ul>
+				<li><p>korekta i edycja pism</p></li>
+				<li><p>tłumaczenie dokumentów i pomoc w ich zrozumieniu</p></li>
+				<li><p>odpowiadanie na pisma urzędowe</p></li>
+				<li><p>pomoc sprawach publicznych</p></li>
+				<li><p>przepisywanie pism</p></li>
+				<li><p>sprawdzenie przygotowanej dokumentacji</p></li>
+			</ul>
+		</section>
+	</article>
+</template>
