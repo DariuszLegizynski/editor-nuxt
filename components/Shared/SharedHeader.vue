68fbec97b34f2646dcf68c1604a89393539c167d
Dariusz Legizynski <d.legizynski@.com>
2024-06-03 11:00:57 +0200

    adjust header style

--- components/Shared/SharedHeader.vue
+++ components/Shared/SharedHeader.vue
@@ -1,34 +1,23 @@
 <script setup lang="ts">
-  const isBurgerActive = ref<boolean>(false)
+	const isBurgerActive = ref<boolean>(false)
 </script>
 
 <template>
-  <nav class="fixed top-0 left-0 flex justify-between items-center px-2 z-10 h-12 bg-white w-full">
-    Logo
-    <BaseBurger
-      @click="isBurgerActive = !isBurgerActive"
-      :active="isBurgerActive"
-    />
-    <aside
-		class="transform transition-transform duration-500 ease-in-out right-0 top-12 fixed bg-primary bg-opacity-50 w-full py-4 z-10"
-		:class="{ '-translate-y-40': !isBurgerActive }"
-	>
-		<ul class="pl-4 flex flex-col items-center">
-			<li class="py-2">
-				<BaseButton
-					variant="navbar"
-					class="text-white font-semibold"
-					><p>Leistungen</p></BaseButton
-				>
-			</li>
-			<li class="py-2">
-				<BaseButton
-					variant="navbar"
-					class="text-white font-semibold"
-					><p>Über mich</p></BaseButton
-				>
-			</li>
-		</ul>
-	</aside>
-  </nav>
+	<nav class="fixed top-0 left-0 flex justify-between items-center px-2 z-10 h-12 bg-primary bg-opacity-90 w-full">
+		<NuxtLink to="/"><p class="text-white">Logo</p></NuxtLink>
+		<BaseBurger @click="isBurgerActive = !isBurgerActive" :active="isBurgerActive" />
+		<aside
+			class="transform transition-transform duration-500 ease-in-out right-0 top-12 fixed bg-primary bg-opacity-90 w-full py-4 z-10"
+			:class="{ '-translate-y-44': !isBurgerActive }"
+		>
+			<ul class="pl-4 flex flex-col items-center">
+				<li class="py-2">
+					<BaseButton variant="navbar" class="text-white font-semibold"><div class="h4">Usługi</div></BaseButton>
+				</li>
+				<li class="py-2">
+					<BaseButton variant="navbar" class="text-white font-semibold"><div class="h4">O Mnie</div></BaseButton>
+				</li>
+			</ul>
+		</aside>
+	</nav>
 </template>
