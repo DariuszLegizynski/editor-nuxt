4c26eb20c1d0f31a6df358524fff00ff34cf19b1
Dariusz Legizynski <d.legizynski@.com>
2024-06-06 21:57:01 +0200

    fix tailwind custom class import

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	css: ["~/assets/css/tailwind.css"],
+	css: ["~/assets/css/global.css", "~/assets/css/tailwind.css"],
 	devtools: { enabled: true },
 	modules: ["@nuxtjs/tailwindcss", "@nuxtjs/strapi", "@nuxt/image"],
 	image: {
